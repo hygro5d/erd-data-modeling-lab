f321fbda6463c40277509ffd4e684cf8a92f75cc
hygro5d <faidhiamran@Faidhis-MacBook-Pro.local>
2018-06-29 10:51:37 +0800

    done

---
+++ seed.sql
@@ -0,0 +1,9 @@
+INSERT INTO users (name, phone, email) VALUES ('faidhi', 94575537, 'faidhi@amran.com')
+
+INSERT INTO articles (title, description, rating, user_id) VALUES ('Jurassic Park', 'dinosaurs everywhere', 5,)
+
+INSERT INTO articles (title, description, rating, user_id) VALUES ('Toys Story', 'toys everywhere', 5)
+
+INSERT INTO users_articles (user_id, articles_id) (1, 1)
+
+INSERT INTO users_articles (user_id, articles_id) (1, 2)
